70bb6a70fe8a2ca69bef4cb39e7a2da6505ebe07
MatsKruijer <matskruijerhhw@gmail.com>
2025-07-29 15:15:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from transformers import pipeline
 
 app = Flask(__name__)
-summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+summarizer = pipeline("summarization", model="philschmid/bart-mini-xsum-samsum")
 
 @app.route("/", methods=["POST"])
 def summarize():
